d0a0ff9d24f3079b6d026b9ac5b44735f2d8169a
Andy Mee <andymee2000@gmail.com>
2024-03-11 13:24:25 -0400

    Complete page 2 and various changes

--- lib/main.dart
+++ lib/main.dart
@@ -32,29 +32,52 @@ class MyAppState extends ChangeNotifier {
   final TextEditingController instructionsField = TextEditingController();
   final TextEditingController ingredientsField = TextEditingController();
 
-  String? title;
-  String? instructions;
+  String title = '';
+  String instructions = '';
   var ingredients = <String>[];
   var tags = <String>[];
+  
 
   @override
   void dispose() {
     // Clean up the controllers when the widget is disposed.
     titleField.dispose();
     instructionsField.dispose();
+    ingredientsField.dispose();
     super.dispose();
   }
 
   void addIngredient(){
     if (!ingredients.contains(ingredientsField.text)){
       ingredients.add(ingredientsField.text);
+      print("Added");
     }
+    notifyListeners();
   }
 
   void removeIngredient(){
     if(ingredients.isNotEmpty){
       ingredients.removeLast();
+      print("Removed");
     }
+    notifyListeners();
+  }
+
+void submit(){
+  title = titleField.text;
+  instructions = instructionsField.text;
+}
+
+void clear(){
+  title = '';
+  instructions = '';
+  ingredients.clear();
+}
+
+  @override
+  void notifyListeners() {
+    notifyListeners();
+    
   }
 }
 
@@ -175,14 +198,27 @@ class MakeRecipe extends StatelessWidget{
               ),
             ],
           ),
-          ListView(
-            children: [
-              for (var ing in appState.ingredients)
-                ListTile(
-                  leading: Icon(Icons.add),
-                  title: Text(ing),
-                ),
-            ],
+          Expanded(
+            child: 
+              ListView.builder(
+                itemCount: appState.ingredients.length,
+                itemBuilder: (BuildContext ctxt, int index) {
+                  return Text(
+                    appState.ingredients[index],
+                    key: UniqueKey()
+                  );
+                },
+                
+              ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: ElevatedButton(
+              onPressed: (){
+                appState.submit();
+              },
+              child: Text('Submit'),
+            ),
           ),
         ],
       ),
@@ -194,8 +230,34 @@ class ViewRecipe extends StatelessWidget{
   @override
   Widget build(BuildContext context){
     var appState = context.watch<MyAppState>();
-    return Center(
-      child: Text('Page2')
+     if (appState.title.isEmpty){
+      return Center(
+        child: Text('Sorry, nothing.'),
+        );
+    }
+
+    return ListView(
+      children: [
+        
+        Text(appState.title),
+        Text('Ingredients'),
+        for (var ingr in appState.ingredients)
+          ListTile(
+            leading: Icon(Icons.add),
+            title: Text(ingr),
+          ),
+        Text('Preparation Instructions'),
+        ListTile(
+          leading: Icon(Icons.add),
+          title: Text(appState.instructions),
+        ),
+        ElevatedButton(
+          onPressed: (){
+            appState.clear();
+          },
+           child: Text('Clear'),
+        ),
+      ],
     );
   }
 }
