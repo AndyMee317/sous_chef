ad2269d6e4e864f175755bc87b8dda28b6f52037
Andy Mee <andymee2000@gmail.com>
2024-04-22 01:42:33 -0400

    (Most of) Make recipe front end complete

--- lib/pages/make_recipe_page.dart
+++ lib/pages/make_recipe_page.dart
@@ -13,9 +13,45 @@ class MakeRecipePage extends StatefulWidget{
 class _MakeRecipePageState extends State<MakeRecipePage> {
 
   final TextEditingController titleField = TextEditingController();
+  final TextEditingController instructionsField = TextEditingController();
+  final TextEditingController ingredientsField = new TextEditingController();
 
+  List<String> ingredients = [];
   void logout() {
     FirebaseAuth.instance.signOut(); 
+  }
+
+  void addIngredient(){
+
+    if(ingredientsField.text.isEmpty){
+      showDialog(
+          context: context,
+          builder: (context) => AlertDialog(
+            title: Text("Please enter something in the field")
+          )
+        );
+    }
+
+    if(!ingredients.contains(ingredientsField.text)){
+      ingredients.add(ingredientsField.text);
+      ingredientsField.clear();
+      setState((){});
+    }
+    else{
+      showDialog(
+        context: context,
+        builder: (context) => AlertDialog(
+          title: Text("Ingredient already added")
+        )
+      );
+    }
+  }
+
+  void removeIngredient(){
+    if(ingredients.isNotEmpty){
+      ingredients.removeLast();
+      setState((){});
+    }
   }
 
   @override 
@@ -24,7 +60,7 @@ class _MakeRecipePageState extends State<MakeRecipePage> {
   Widget build(BuildContext context){
     return DefaultTabController(
       initialIndex: 0,
-      length: 2,
+      length: 4,
       child: Scaffold(
         appBar: AppBar(
           title: Text("Make Recipe"),
@@ -40,6 +76,8 @@ class _MakeRecipePageState extends State<MakeRecipePage> {
             tabs: <Widget>[
               Tab(icon: Icon(Icons.filter_1 )),
               Tab(icon: Icon(Icons.filter_2)),
+              Tab(icon: Icon(Icons.filter_3)),
+              Tab(icon: Icon(Icons.filter_4)),
             ],
           ),
         ),
@@ -49,7 +87,7 @@ class _MakeRecipePageState extends State<MakeRecipePage> {
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  Text('Title'),
+                  Text('1. Title'),
                   Padding(
                     padding: EdgeInsets.all(26.0),
                     child: TextField(
@@ -64,7 +102,79 @@ class _MakeRecipePageState extends State<MakeRecipePage> {
               ),
             ),
             Center(
-              child: Text("Lorem ipsum2"),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text('2. Preparation instructions'),
+                  Padding(
+                    padding: const EdgeInsets.all(26.0),
+                    child: SizedBox(
+                      child: TextField(
+                        controller: instructionsField,
+                        maxLines: 5,
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                          hintText: 'Explain how to prepare your recipe',
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children:[
+                  Text("3. List of ingredients"),
+                  Padding(
+                    padding: const EdgeInsets.all(26.0),
+                    child: TextField(
+                      controller: ingredientsField,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        hintText: 'Enter ingredient and amount'
+                      ),
+                      onSubmitted: (text){
+                        addIngredient();
+                      },
+                    ),
+                  ),
+                  Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children:[
+                      ElevatedButton(
+                        onPressed: (){
+                          addIngredient();
+                        },
+                        child: Text('Add'),
+                      ), 
+                      ElevatedButton(
+                        onPressed: (){
+                          removeIngredient();
+                        },
+                        child: Text('Remove'),
+                      ),
+                      ],
+                    ),
+                  new Expanded(
+                    child: new ListView.builder(
+                      itemCount: ingredients.length,
+                      itemBuilder: (BuildContext ctxt, int Index){
+                        return new Text("\u2022 " + ingredients[Index]);
+                      }
+                    ) 
+                  ),
+                ],
+              ),
+            ),
+            Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text("Images coming soon")
+                ],
+              ),
             ),
           ],
         ),
