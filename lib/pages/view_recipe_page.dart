018f997a8d8192cc559de58e1e8a8b24341d5875
Andy Mee <andymee2000@gmail.com>
2024-04-26 19:13:56 -0400

    remove unneeded include file in view recipe page

--- lib/pages/view_recipe_page.dart
+++ lib/pages/view_recipe_page.dart
@@ -2,7 +2,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:sous_chef/database/firestore.dart';
 
 class ViewRecipePage extends StatefulWidget{
